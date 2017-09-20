3d61fa8497bd3b41f99b65bfaa0646593dec2bd2
georgeo15 <bumfuzzlers@gmail.com>
2017-09-20 11:11:03 +0300

    table update

--- tb_gallery.sql
+++ tb_gallery.sql
@@ -1,30 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: mysql.metropolia.fi
--- Generation Time: Sep 20, 2017 at 10:40 AM
--- Server version: 10.1.23-MariaDB
--- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `test`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_gallery`
---
 
 CREATE TABLE `tb_gallery` (
   `pic_id` int(50) NOT NULL,
